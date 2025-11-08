b5698d90c53926fcc681f84f66a7d7ca9597b8e8
Evan Lee <evanwarnerlee@gmail.com>
2025-11-08 10:29:50 -0700

    bassci lackjack

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function putStuffOnCard(id, stuff) {
   card.children[0].src = stuff.img;
   card.children[1].innerText = stuff.title;
   card.children[2].innerHTML = stuff.text;
-  if (id == "main-card") {
+  if (id === "main-card") {
     card.children[3].onclick = () => {
       window.open(stuff.site, '_blank').focus();
     }
@@ -19,8 +19,7 @@ async function getGithubData(owner, repo) {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
     const data = await response.json();
     const updatedAt = new Date(data.updated_at);
-    const dateOnly = updatedAt.toISOString().split("T")[0];
-    return dateOnly;
+    return updatedAt.toISOString().split("T")[0];
   } catch (error) {
     console.error("GitHub API error:", error);
     return false;
@@ -40,8 +39,11 @@ const yeOlde = {
   img: "eye.png",
   title: "YE OLDE",
   text: `
-  Build a deck, swing a rusty blade, and wander endless halls full of monsters and merchants. 
-  Some of them were heroes once. You might be too. If you draw the right card.`,
+  Fight potentially lethal monsters! 
+  Collect loot and treasure!
+  Upgrade your weapons in safe, friendly-looking shops!
+  Die!
+  `,
   site: "https://evanworks.github.io/ye-olde",
   repo: ["evanworks", "ye-olde"]
 }
@@ -49,7 +51,7 @@ const carrotClicker = {
   img: "box.png",
   title: "CARROT CLICKER",
   text: `
-  A low-stress farming game for high-stress humans. Slow growth, crisp graphics, and a lovely feeling of productivity.`,
+  A simple farming game. Slowly grow carrots while you grow your farm, and enjoy the numerous jokes along the way.`,
   site: "https://evanworks.github.io/carrot-clicker",
   repo: ["evanworks", "carrot-clicker"]
 } 
@@ -57,28 +59,36 @@ const pythonFishing = {
   img: "sunset.png",
   title: "PYTHON FISHING",
   text: `
-  Fish for oddities, chat with locals. Upgrade your tools, enjoy the retro graphics, and lose track of time. Fall in the lake...more than once.`,
+  Fish for oddities, chat with locals. Upgrade your rod, enjoy the simple graphics, and lose track of time.`,
   site: "https://evanworks.github.io/fishing",
   repo: ["evanworks", "fishing"]
+}
+const asciiBlackjack = {
+  img: "poker.png",
+  title: "ASCII BLACKJACK",
+  text: `
+  Gamble away your life savings, or dominate the poker table by blatantly cheating. Upgrade your cards and learn unique playstyles.`,
+  site: "https://evanworks.github.io/ascii-blackjack",
+  repo: ["evanworks", "ascii-blackjack"]
 }
 
 putStuffOnCard("main-card", carrotClicker);
 putStuffOnCard("left", yeOlde);
 
-const cardOrder = [littleWorld, carrotClicker, pythonFishing, yeOlde]
+const cardOrder = [carrotClicker, littleWorld,  pythonFishing, asciiBlackjack, yeOlde];
 
 function pickCorrectCard(currentCard) {
   console.log(currentCard);
-  leftCard = cardOrder[(currentCard + 1) % cardOrder.length];
-  mainCard = cardOrder[currentCard]
-  rightCard = cardOrder[(currentCard - 1 + cardOrder.length) % cardOrder.length];
+  let leftCard = cardOrder[(currentCard + 1) % cardOrder.length];
+  let mainCard = cardOrder[currentCard]
+  let rightCard = cardOrder[(currentCard - 1 + cardOrder.length) % cardOrder.length];
 
   putStuffOnCard("left", leftCard);
   putStuffOnCard("main-card", mainCard)
   putStuffOnCard("right", rightCard)
 }
 
-correctCard = 0;
+let correctCard = 0;
 
 pickCorrectCard(correctCard)
 
